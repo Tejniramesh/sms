d02fa28d157d78fe880159cb8dddd9901a52e21f
Tejni <tejni.gr.2024.it@rajalakshmi.edu.in>
2026-05-15 20:45:25 +0530

    Fixed deployment database config

--- utils/db.py
+++ utils/db.py
@@ -5,20 +5,28 @@ import os
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
+try:
+    MONGO_URI = os.getenv("MONGO_URI")
 
-if not MONGO_URI:
-    MONGO_URI = st.secrets["MONGO_URI"]
+    if not MONGO_URI:
+        MONGO_URI = st.secrets["MONGO_URI"]
 
-DB_NAME = os.getenv("DB_NAME")
+    DB_NAME = os.getenv("DB_NAME")
 
-if not DB_NAME:
-    DB_NAME = st.secrets["DB_NAME"]
+    if not DB_NAME:
+        DB_NAME = st.secrets["DB_NAME"]
 
-client = MongoClient(MONGO_URI)
+    client = MongoClient(
+        MONGO_URI,
+        serverSelectionTimeoutMS=5000
+    )
 
-db = client[DB_NAME]
+    db = client[DB_NAME]
 
-users_collection = db["users"]
-contacts_collection = db["contacts"]
-messages_collection = db["messages"]
+    users_collection = db["users"]
+    contacts_collection = db["contacts"]
+    messages_collection = db["messages"]
+
+except Exception as e:
+
+    st.error(f"Database Connection Error: {e}")
