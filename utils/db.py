a104b8d4012d4de1882b65b270595f6a210a58bf
Tejni <tejni.gr.2024.it@rajalakshmi.edu.in>
2026-05-15 20:25:43 +0530

    Fixed MongoDB deployment

--- utils/db.py
+++ utils/db.py
@@ -1,11 +1,19 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import streamlit as st
 import os
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
+
+if not MONGO_URI:
+    MONGO_URI = st.secrets["MONGO_URI"]
+
 DB_NAME = os.getenv("DB_NAME")
+
+if not DB_NAME:
+    DB_NAME = st.secrets["DB_NAME"]
 
 client = MongoClient(MONGO_URI)
 
